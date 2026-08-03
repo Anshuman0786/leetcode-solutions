aeba8e7d622ddf8117ceff0ad2d4a00304cde8fb
Anshuman Nigam <106136361+Anshuman0786@users.noreply.github.com>
2026-08-03 14:34:25 +0530

    Time: 63 ms (61.54%) | Memory: 14.2 MB (71.21%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-    int solve(int index, int prevIndex, vector<int>& nums, vector<vector<int>>& dp)
-    {
-        if (index == nums.size())
-        {
-            return 0;
-        }
-
-        // shift prevIndex by 1 so we can store -1 case as index 0 in dp
-        if (dp[index][prevIndex + 1] != -1)
-        {
-            return dp[index][prevIndex + 1];
-        }
-
-        // option 1: skip current element
-        int notTake = solve(index + 1, prevIndex, nums, dp);
-
-        // option 2: take current element, only if it extends the subsequence
-        int take = 0;
-        if (prevIndex == -1 || nums[index] > nums[prevIndex])
-        {
-            take = 1 + solve(index + 1, index, nums, dp);
-        }
-
-        dp[index][prevIndex + 1] = max(take, notTake);
-        return dp[index][prevIndex + 1];
-    }
-
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        vector<int> dp(n, 1); // every single element is an LIS of length 1 by itself
 
-        return solve(0, -1, nums, dp);
+        int maxLen = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = max(maxLen, dp[i]);
+        }
+        return maxLen;
     }
 };
