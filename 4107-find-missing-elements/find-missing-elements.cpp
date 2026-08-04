aac7cb71b1318220d46bd6b5eb97515683b8b03e
Anshuman Nigam <106136361+Anshuman0786@users.noreply.github.com>
2026-08-04 14:11:05 +0530

    Time: 4 ms (35.49%) | Memory: 32.3 MB (54.61%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
+        if (nums.empty()) return {};
+
         int mn = *min_element(nums.begin(), nums.end());
         int mx = *max_element(nums.begin(), nums.end());
-        unordered_map<int,bool> mp;
-        vector<int> miss;
 
-        for(int x : nums)
-        {
-            mp[x] = true;
+        vector<bool> present(mx - mn + 1, false);
+        for (int x : nums) {
+            present[x - mn] = true;
         }
 
-        for(int i = mn; i <= mx; i++)
-        {
-            if(mp[i] == false)
-            {
-                miss.push_back(i);
+        vector<int> miss;
+        for (int i = 0; i < (int)present.size(); i++) {
+            if (!present[i]) {
+                miss.push_back(mn + i);
             }
         }
         return miss;
